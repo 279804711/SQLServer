d8abe968cfc4c0158192f5f278ecd607fbafb95d
ShenYanjie <279804711@qq.com>
2025-04-10 00:26:42 +0800

    SQL order commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+--查询
+select * from Table_1 
+--新增
+insert into Table_1( UserName, PassWord) values('222', '222')
+--删除
+delete from Table_1 where UserName='222'
+--修改
+update Table_1 set UserName='777' where (NickName='333' AND PassWord='222')
+
+
+
